37bde5decd2c6f702e13a2b94a74bcabe0a32d49
Thata <126881634+Thanaha@users.noreply.github.com>
2023-06-23 08:56:39 -0300

    Add files via upload

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+console.log("Meu Primeiro Programa: trabalhando com variáveis");
+
+const idade =16;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);    
+console.log(idade-2);    
+console.log(idade/2);    
+
+const idadeSomada = idade+2
+console.log(idadeSomada);
