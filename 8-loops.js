3a6f56666741e5f72e5f98dbb51a156ec288bc0a
Thata <126881634+Thanaha@users.noreply.github.com>
2023-08-01 09:54:54 -0300

    Add files via upload

--- 8-loops.js
+++ 8-loops.js
@@ -1,46 +1,28 @@
-console.log(`Trabalhando com listas`);
+console.log(`\nTrabalhando com condicionais`);
 const listaDeDestinos = new Array(
     `Salvador`,
     `São Paulo`,
-    `Rio de Janeiro`,
+    `Rio de Janeiro`
 );
 
 const idadeComprador = 18;
 const estaAcompanhada = false;
-const temPassagemComprada = true;
+let temPassagemComprada = false;
+const destino = "Salvador";
 
-console.log("Destinos possíveis:");
+console.log("\n Destinos possíveis:");
 console.log(listaDeDestinos);
 
-// if (idadeComprador >= 18) {
-//    console.log("Comprador maior de idade");
-//    listaDeDestinos.splice(1,1); //removendo item
-// } else if (estaAcompanhada == true) {
-//    console.log("Comprador está acompanhado");
-//    listaDeDestinos.splice(1,1) //removendo item
-// } else {
-//    console.log("Não é maior de Idade e não vender");
-// }
+const podeComprar   = idadeComprador >= 18 || estaAcompanhada == true;
 
-if (idadeComprador >= 18 || estaAcompanhada == true) {
-   console.log("Boa Viagem!!");
-   listaDeDestinos.splice(2,1); //removendo item
-} else {
-    console.log("Não é maior de Idade e não posso vender");
+let contador = 0;
+let destinoExiste = false;
+while(contador<3){
+    if(listaDeDestinos[contador] == destino){
+        destinoExiste = true;
+        break;
+    }
+    contador += 1;
 }
 
-console.log("Embarque: \n\n")
-if(idadeComprador >= 18 && temPassagemComprada){
-    console.log("Boa Viagem");
-}else{
-    console.log("Você não pode embarcar");
-}
-
-
-console.log(listaDeDestinos);
-
-//console.log(idadeComprador > 18);
-//console.log(idadeComprador < 18);
-//console.log(idadeComprador <= 18);
-//console.log(idadeComprador >= 18);
-//console.log(idadeComprador == 18);
+console.log("Destino existe: ", destinoExiste);
